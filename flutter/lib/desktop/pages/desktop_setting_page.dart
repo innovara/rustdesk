38ba15550ba395f239126e1dbfa46a57c7da51e4
Manuel Fombuena <fombuena@outlook.com>
2026-01-12 22:31:40 +0000

    Enable remote access to config by default
    [skip ci]
    
    Signed-off-by: Manuel Fombuena <mfombuena@innovara.tech>

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1125,7 +1125,7 @@ class _SafetyState extends State<_Safety> with AutomaticKeepAliveClientMixin {
                   context, 'Enable privacy mode', kOptionEnablePrivacyMode,
                   enabled: enabled, fakeValue: fakeValue),
             _OptionCheckBox(context, 'Enable remote configuration modification',
-                kOptionAllowRemoteConfigModification,
+                kOptionEnableRemoteConfigModification,
                 enabled: enabled, fakeValue: fakeValue),
           ],
         ),
